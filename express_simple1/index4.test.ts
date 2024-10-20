de247ba8760900cafc009b096f0688cfabc97334
Ubuntu <ubuntu@ip-172-31-55-85.ap-northeast-2.compute.internal>
2024-10-20 05:14:41 +0000

    몇개 추가

---
+++ express_simple1/index4.test.ts
@@ -0,0 +1,20 @@
+
+// 6. 테스트 코드 작성
+// __tests__/app.test.ts
+// npm i jest supertest
+import request from 'supertest';
+import app from './index4';
+
+describe('AppController (e2e)', () => {
+  it('GET / should return Hello, World!', async () => {
+    const response = await request(app).get('/');
+    expect(response.status).toBe(200);
+    expect(response.text).toBe('Hello, World!');
+  });
+
+  it('GET /test should return Test', async () => {
+    const response = await request(app).get('/test');
+    expect(response.status).toBe(200);
+    expect(response.text).toBe('Test');
+  });
+});
