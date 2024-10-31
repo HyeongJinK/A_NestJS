d6c83016482afee7e37612a1850677ec91531a58
HyeongJinK <wkzkfmxk23@gmail.com>
2024-10-31 13:59:55 +0900

    constants.ts 추가 메타데이터 Key값 저장

--- clone_nestjs1/src/decorators/controller.decorator.ts
+++ clone_nestjs1/src/decorators/controller.decorator.ts
@@ -1,10 +1,13 @@
 import 'reflect-metadata';
+import {PATH_METADATA} from "../common/constants";
 
+/**
+ * @param prefix URL 경로
+ * */
 export function Controller(prefix?: string): ClassDecorator {
     const path = prefix ? prefix : '/';
 
     return (target: object) => {
-        // console.log('target:', target);
-        Reflect.defineMetadata('path', path, target);
+        Reflect.defineMetadata(PATH_METADATA, path, target);
     }
 }
--- clone_nestjs1/test/decorators/controller.decorator.test.ts
+++ clone_nestjs1/test/decorators/controller.decorator.test.ts
@@ -1,4 +1,5 @@
 import {Controller} from "../../src/decorators/controller.decorator";
+import {PATH_METADATA} from "../../src/common/constants";
 
 @Controller("/test")
 class TestController {
@@ -9,7 +10,7 @@ class TestController {
 
 describe("Controller Decorator", () => {
     test("path 메타데이터 설정값 확인", () => {
-        let path = Reflect.getMetadata("path", TestController);
+        let path = Reflect.getMetadata(PATH_METADATA, TestController);
         expect(path).toBe("/test");
     });
 });
